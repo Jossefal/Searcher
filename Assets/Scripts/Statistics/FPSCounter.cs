56c712a17ca0f1e64099ef7964ed32373f99a931
Jossefal <maks.snitsarenko@gmail.com>
2020-04-04 16:07:07 +0300

    Add FPS counter and create prefab of stars background

---
+++ Assets/Scripts/Statistics/FPSCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    public float updateInterval = 0.5F;
+    public Text text;
+
+    private double lastInterval; 
+    private int frames = 0;
+    private double fps;
+
+    void Start()
+    {
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+
+    void Update()
+    {
+        frames++;
+        float timeNow = Time.realtimeSinceStartup;
+
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = (float)(frames / (timeNow - lastInterval));
+            frames = 0;
+            lastInterval = timeNow;
+
+            if(text != null)
+                text.text = Converter.ConvertToString(Math.Round(fps, 1));
+        }
+    }
+}
