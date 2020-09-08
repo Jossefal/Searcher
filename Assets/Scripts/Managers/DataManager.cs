2385a89908f85efa771dacd7fd84b2742aa0c597
Jossefal <maks.snitsarenko@gmail.com>
2020-09-08 17:45:53 +0300

    Fix skin 1

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -86,9 +86,8 @@ public static class DataManager
     internal static event Action onDataChanged;
 
     internal static bool isDataLoaded { get; private set; }
-
-    internal static bool isTestMode { get; private set; } = true;
-    internal static bool isLocalTestMode { get; private set; } = false;
+    internal static bool isTestMode { get; private set; } = false;
+    internal static bool isLocalTestMode { get; private set; } = true;
 
     static DataManager()
     {
